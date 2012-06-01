8496d2f11717418c86a3b30632e62a19539e3166
gorodenco <nelugorodenco@gmail.com>
2012-06-01 10:13:46 +0200

    Ajout des interface de properties

---
+++ ldap-browser/src/main/java/fr/uparis10/miage/ldap/client/enums/EnumOrgUnitAttrMessages.java
@@ -0,0 +1,113 @@
+/**
+ * This file is part of LDAP Web Browser project
+ * Copyright (C) 2012 gorodenco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Creation date: 27 mai 2012
+ */
+package fr.uparis10.miage.ldap.client.enums;
+
+import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * @author auprotois
+ * 
+ **/
+
+@DefaultLocale("fr")
+public interface EnumOrgUnitAttrMessages extends Messages {
+
+	@Key("objectClass")
+	String getObjectClass();
+
+	@Key("ou")
+	String getOu();
+
+	@Key("userPassword")
+	String getUserPassword();
+
+	@Key("searchGuide")
+	String getSearchGuide();
+
+	@Key("seeAlso")
+	String getSeeAlso();
+
+	@Key("businessCategory")
+	String getBusinessCategory();
+
+	@Key("x121Address")
+	String getX121Address();
+
+	@Key("registeredAddress")
+	String getRegisteredAddress();
+
+	@Key("destinationIndicator")
+	String getDestinationIndicator();
+
+	@Key("preferredDeliveryMethod")
+	String getPreferredDeliveryMethod();
+
+	@Key("telexNumber")
+	String getTelexNumber();
+
+	@Key("teletexTerminalIdentifier")
+	String getTeletexTerminalIdentifier();
+
+	@Key("telephoneNumber")
+	String getTelephoneNumber();
+
+	@Key("internationaliSDNNumber")
+	String getInternationaliSDNNumber();
+
+	@Key("facsimileTelephoneNumber")
+	String getFacsimileTelephoneNumber();
+
+	@Key("street")
+	String getStreet();
+
+	@Key("postOfficeBox")
+	String getPostOfficeBox();
+
+	@Key("postalCode")
+	String getPostalCode();
+
+	@Key("postalAddress")
+	String getPostalAddress();
+
+	@Key("physicalDeliveryOfficeName")
+	String getPhysicalDeliveryOfficeName();
+
+	@Key("st")
+	String getSt();
+
+	@Key("l")
+	String get1();
+
+	@Key("description")
+	String getDescription();
+
+	@Key("supannCodeEntite")
+	String getSupannCodeEntite();
+
+	@Key("supannTypeEntite")
+	String getSupannTypeEntite();
+
+	@Key("supannCodeEntiteParent")
+	String getSupannCodeEntiteParent();
+
+	@Key("supannRefId")
+	String getSupannRefId();
+
+}
